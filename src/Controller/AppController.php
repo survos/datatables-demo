cf451f7a715f3f2225d96df7384266165be74987
Tac Tacelosky <tacman@gmail.com>
2021-02-25 12:06:05 -0500

    update to Symfony 5.2

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,19 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AppController extends Controller
+class AppController extends AbstractController
 {
     /**
      * @Route("/", name="app")
      */
-    public function index()
+    public function index(string $projectDir)
     {
         return $this->render('app/index.html.twig', [
-            'js' => file_get_contents($this->get('kernel')->getProjectDir() . "/assets/js/dynamic-table.js"),
-            'appjs' => file_get_contents($this->get('kernel')->getProjectDir() . "/assets/js/app.js"),
+            'js' => file_get_contents($projectDir. "/assets/js/dynamic-table.js"),
+            'appjs' => file_get_contents($projectDir . "/assets/js/app.js"),
             'controller_name' => 'AppController',
         ]);
     }
